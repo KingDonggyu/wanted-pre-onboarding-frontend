dc78078c40c8ddde8c2463ec76e03010db887dd6
KingDonggyu <dgkim69166916@gmail.com>
2023-06-11 17:48:12 +0900

    feat(#12): 404 페이지 UI 구현

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -1,3 +1,13 @@
-const NotFoundPage = () => <main>404</main>;
+import { Link } from 'react-router-dom';
+import * as s from './style';
+import Route from '../../constants/routes';
+
+const NotFoundPage = () => (
+  <s.Wrapper>
+    <s.Title>해당 페이지를 찾을 수 없습니다.</s.Title>
+    <s.Description>404 - Page is not Found</s.Description>
+    <Link to={Route.HOME}>홈으로 이동</Link>
+  </s.Wrapper>
+);
 
 export default NotFoundPage;
